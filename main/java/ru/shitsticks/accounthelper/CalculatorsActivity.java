e97501dfd8048917a02152a519e022640d14e711
hydrophonyx <hydrophonyx@gmail.com>
2018-07-09 18:07:05 +0300

    работает калькулятор ндс

--- main/java/ru/shitsticks/accounthelper/CalculatorsActivity.java
+++ main/java/ru/shitsticks/accounthelper/CalculatorsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,10 +12,13 @@ import java.math.BigDecimal;
 
 public class CalculatorsActivity extends AppCompatActivity {
 
+    public final static String TAG = "AccLogger";
+    private static final BigDecimal BigDecHundred = new BigDecimal(100);
     EditText etxSumNoNDS;
     EditText etxTaxNDS;
     EditText etxValueNDS;
     EditText etxSumNDS;
+    static int moneyRound = BigDecimal.ROUND_UP;
     int sourceNDS; //ресурс заполненный пользователем, что б понять какое значение вычислять при изменении ндс
     int mOnFocusNDS; //проверка фокуса на едиттекст, иначе срабатыевает onchangelistener
 
@@ -31,6 +35,7 @@ public class CalculatorsActivity extends AppCompatActivity {
         etxTaxNDS.setText(R.string.NDS);
 
         final Toast toast = Toast.makeText( this, "Заполните одно из полей", Toast.LENGTH_LONG);
+        final Toast toastZero = Toast.makeText(this, "Процент должен быть положительным числом отличным от ноля", Toast.LENGTH_LONG);
 
         View.OnFocusChangeListener mOnFocusListener = new View.OnFocusChangeListener() {
                 @Override
@@ -40,11 +45,11 @@ public class CalculatorsActivity extends AppCompatActivity {
                             switch (v.getId()){
                                 case R.id.NDS_calc_sum:
                                     mOnFocusNDS = R.id.NDS_calc_sum;
-                                    PercentCalc.noEmptyFocus(etxTaxNDS, v);
+                                    PercentCalc.noEmptyFocus(etxTaxNDS, v, toastZero);
                                     break;
                                 case R.id.NDS_calc_sum_no_tax:
                                     mOnFocusNDS = R.id.NDS_calc_sum_no_tax;
-                                    PercentCalc.noEmptyFocus(etxTaxNDS, v);
+                                    PercentCalc.noEmptyFocus(etxTaxNDS, v, toastZero);
                                     break;
                                 case R.id.NDS_calc_tax:
                                     mOnFocusNDS = R.id.NDS_calc_tax;
@@ -83,15 +88,20 @@ public class CalculatorsActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
 
-                if (s.toString().isEmpty()) {}
-                else {
+
+
+                if (!s.toString().isEmpty()){
                     if (mOnFocusNDS == etxSumNoNDS.getId()) {
                         sourceNDS = R.id.NDS_calc_sum_no_tax;
-                        float sumNoNDS = Float.parseFloat(s.toString());
-                        float nds = Float.parseFloat(etxTaxNDS.getText().toString());
-                        float sumNDS = PercentCalc.getSumWithPercent(sumNoNDS, nds);
-                        float valueNDS = PercentCalc.getPercentValue(sumNDS,sumNoNDS);
-                        etxSumNDS.setText(String.valueOf(sumNoNDS));
+                        BigDecimal sumNoNDS = new BigDecimal(s.toString());
+                        sumNoNDS.setScale(2, moneyRound);
+                        BigDecimal nds = new BigDecimal(etxTaxNDS.getText().toString());
+                        nds.setScale(2, moneyRound);
+                        BigDecimal sumNDS = PercentCalc.getSumWithPercent(sumNoNDS, nds);
+                        sumNDS.setScale(2, moneyRound);
+                        BigDecimal valueNDS = PercentCalc.getPercentValue(sumNDS,sumNoNDS);
+                        valueNDS.setScale(2, moneyRound);
+                        etxSumNDS.setText(String.valueOf(sumNDS));
                         etxValueNDS.setText(String.valueOf(valueNDS));
                     }
                 }
@@ -111,22 +121,27 @@ public class CalculatorsActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-
-                    if (!s.toString().isEmpty()) {
-                        float nds = Float.parseFloat(s.toString());
+                EditText sourceText = findViewById(sourceNDS);
+                if (sourceText != null){
+                    if (!s.toString().isEmpty() & !sourceText.getText().toString().isEmpty()) {
+                        BigDecimal nds = new BigDecimal(s.toString());
                         switch (sourceNDS) {
                             case R.id.NDS_calc_sum:
-                                float sumNDS = Float.parseFloat(etxSumNDS.getText().toString());
-                                etxSumNoNDS.setText(String.valueOf(PercentCalc.getSumWithoutPercent(sumNDS, nds)));
+                                BigDecimal sumNDS = new BigDecimal(etxSumNDS.getText().toString());
+                                BigDecimal sumNoNDS = new BigDecimal(PercentCalc.getSumWithoutPercent(sumNDS, nds).floatValue());
+                                etxSumNoNDS.setText(String.valueOf(sumNoNDS));
+                                etxValueNDS.setText(String.valueOf(PercentCalc.getPercentValue(sumNDS, sumNoNDS)));
                                 break;
                             case R.id.NDS_calc_sum_no_tax:
-                                float noNDS = Float.parseFloat(etxSumNoNDS.getText().toString());
-                                etxSumNDS.setText(String.valueOf(PercentCalc.getSumWithPercent(noNDS, nds)));
+                                BigDecimal noNDS = new BigDecimal(etxSumNoNDS.getText().toString());
+                                BigDecimal sumNDS1 = new BigDecimal(PercentCalc.getSumWithPercent(noNDS, nds).floatValue());
+                                etxSumNDS.setText(String.valueOf(sumNDS1));
+                                etxValueNDS.setText(String.valueOf(PercentCalc.getPercentValue(sumNDS1, noNDS)));
                                 break;
                             case R.id.NDS_calc_tax_value:
-                                float valueNDS = Float.parseFloat(etxValueNDS.getText().toString());
-                                float sumNDSfromValueNDS = PercentCalc.gerSumWithPercentFromPercentValue(valueNDS, nds);
-                                float sumNoValueNDS = PercentCalc.getSumWithoutPercentFromPercentValue(valueNDS, nds);
+                                BigDecimal valueNDS = new BigDecimal(etxValueNDS.getText().toString());
+                                BigDecimal sumNDSfromValueNDS = PercentCalc.getSumWithPercentFromPercentValue(valueNDS, nds);
+                                BigDecimal sumNoValueNDS = PercentCalc.getSumWithoutPercentFromPercentValue(valueNDS, nds);
                                 etxSumNDS.setText(String.valueOf(sumNoValueNDS));
                                 etxSumNoNDS.setText(String.valueOf(sumNDSfromValueNDS));
                                 etxValueNDS.setText(String.valueOf(PercentCalc.getPercentValue(sumNDSfromValueNDS, sumNoValueNDS)));
@@ -134,6 +149,7 @@ public class CalculatorsActivity extends AppCompatActivity {
                                 break;
                         }
                     }
+            }
             }
         });
         etxSumNDS.addTextChangedListener(new TextWatcher() {
@@ -152,10 +168,14 @@ public class CalculatorsActivity extends AppCompatActivity {
 
                 if (!s.toString().isEmpty() & mOnFocusNDS == etxSumNDS.getId()) {
                     sourceNDS = R.id.NDS_calc_sum;
-                    float nds = Float.parseFloat(etxTaxNDS.getText().toString());
-                    float sumNDS = Float.parseFloat(s.toString());
-                    float sumNoNDS = PercentCalc.getSumWithoutPercent(sumNDS, nds);
-                    float valueNDS = PercentCalc.getPercentValue(sumNDS,sumNoNDS);
+                    BigDecimal nds = new BigDecimal(etxTaxNDS.getText().toString());
+                    nds.setScale(2, moneyRound);
+                    BigDecimal sumNDS = new BigDecimal(s.toString());
+                    sumNDS.setScale(2, moneyRound);
+                    BigDecimal sumNoNDS = PercentCalc.getSumWithoutPercent(sumNDS, nds);
+                    sumNoNDS.setScale(2, moneyRound);
+                    BigDecimal valueNDS = PercentCalc.getPercentValue(sumNDS,sumNoNDS);
+                    valueNDS.setScale(2, moneyRound);
                     etxSumNoNDS.setText(String.valueOf(sumNoNDS));
                     etxValueNDS.setText(String.valueOf(valueNDS));
 
@@ -179,10 +199,12 @@ public class CalculatorsActivity extends AppCompatActivity {
 
                 if(!s.toString().isEmpty() & mOnFocusNDS == etxValueNDS.getId()){
                     sourceNDS = R.id.NDS_calc_tax_value;
-                    float nds = Float.parseFloat(etxTaxNDS.getText().toString());
-                    float valueNDS = Float.parseFloat(s.toString());
+                    BigDecimal nds = new BigDecimal(etxTaxNDS.getText().toString());
+                    nds.setScale(2, moneyRound);
+                    BigDecimal valueNDS = new BigDecimal(s.toString());
+                    valueNDS.setScale(2, moneyRound);
                     etxSumNoNDS.setText(String.valueOf(PercentCalc.getSumWithoutPercentFromPercentValue(valueNDS, nds)));
-                    etxSumNDS.setText(String.valueOf(PercentCalc.gerSumWithPercentFromPercentValue(valueNDS, nds)));
+                    etxSumNDS.setText(String.valueOf(PercentCalc.getSumWithPercentFromPercentValue(valueNDS, nds)));
                 }
 
             }
@@ -191,29 +213,110 @@ public class CalculatorsActivity extends AppCompatActivity {
 
     private static class PercentCalc {
 
-        static float getPercentValue(float sumWithPercent ,float sumWithoutPecent){
-            return sumWithPercent - sumWithoutPecent;
+        static BigDecimal getPercentValue(BigDecimal sumWithPercent ,BigDecimal sumWithoutPercent){
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"getPercentValue: sumWithPercent " + sumWithPercent.toString() + ", sumWithoutPercent " + sumWithoutPercent.toString());
+            }
+
+            return sumWithPercent.subtract(sumWithoutPercent);
+
         }
 
-        static float getSumWithPercent(float sumWithoutPercent, float percent) {
-            return sumWithoutPercent + (sumWithoutPercent * percent / 100);
+        static BigDecimal getPercentValueFromSumWithoutPercent(BigDecimal sumWithoutPercent,BigDecimal percent){
+
+            BigDecimal sumWP = new BigDecimal(sumWithoutPercent.floatValue());
+            sumWP.setScale(2, moneyRound);
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"getPercentValueFromSumWithoutPercent: sumWithoutPercent " + sumWithoutPercent.toString() + ", percent " + percent.toString() + ", sumWP, " + sumWP.toString());
+            }
+
+            return sumWP.multiply(percent).divide(BigDecHundred, moneyRound);
         }
 
-        static float getSumWithoutPercent(float sumWithPercent, float percent) {
-            return (sumWithPercent * 100) / (100 + percent);
+        static BigDecimal getSumWithPercent(BigDecimal sumWithoutPercent, BigDecimal percent) {
+
+            BigDecimal percentValue = getPercentValueFromSumWithoutPercent(sumWithoutPercent, percent);
+            percentValue.setScale(2, moneyRound);
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"getSumWithPercent: sumWithoutPercent " + sumWithoutPercent.toString() + ", percent " + percent.toString() + ", percentValue, " + percentValue.toString());
+            }
+
+            return sumWithoutPercent.add(percentValue);
+
         }
 
-        static float getSumWithoutPercentFromPercentValue(float value, float percent){
-            return (value / ( percent / 100 ) * 100);
+        static BigDecimal getSumWithoutPercent(BigDecimal sumWithPercent, BigDecimal percent) {
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"getSumWithoutPercent: sumWithPercent " + sumWithPercent.toString() + ", percent " + percent.toString());
+            }
+
+            return (sumWithPercent.multiply(BigDecHundred)).divide(percent.add(BigDecHundred), moneyRound);
+
         }
 
-        static float gerSumWithPercentFromPercentValue(float value, float percent){
-            return (value / ( percent / 100 ) * ( 100 + percent));
+        static BigDecimal getSumWithoutPercentFromPercentValue(BigDecimal value, BigDecimal percent){
+
+            BigDecimal val = new BigDecimal(value.floatValue());
+            val.setScale(2, moneyRound);
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"\n getSumWithoutPercentFromPercentValue: value " + value.toString() + ", percent " + percent.toString() + ", val " + val.toString());
+            }
+
+            BigDecimal answer = new BigDecimal(val.multiply(BigDecHundred).floatValue());
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"answer " + answer.toString());
+            }
+
+            answer = answer.divide(percent, moneyRound);
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"answer " + answer.toString());
+            }
+
+            return answer;
+
         }
 
-        static void noEmptyFocus(EditText text, View textOnFocus) {
+        static BigDecimal getSumWithPercentFromPercentValue(BigDecimal value, BigDecimal percent){
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"\n getSumWithPercentFromPercentValue: value " + value.toString() + ", percent " + percent.toString());
+            }
+
+            BigDecimal divider = new BigDecimal(percent.floatValue());
+            divider.setScale(2, moneyRound);
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"percentForComp " + divider.toString());
+            }
+
+            BigDecimal multiplier = new BigDecimal(percent.add(BigDecHundred).floatValue());
+            multiplier.setScale(2, moneyRound);
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"multiplier " + multiplier.toString());
+            }
+
+            value = value.multiply(multiplier);
+
+            if (BuildConfig.DEBUG){
+                Log.i(TAG,"value " + value.toString());
+            }
+
+            return value.divide(divider, moneyRound);
+
+        }
+
+        static void noEmptyFocus(EditText text, View textOnFocus, Toast toast) {
             if (text.getText().length() <= 0) {
-                text.setText("0");
+                toast.show();
+                text.setText("1");
                 textOnFocus.clearFocus();
                 text.requestFocus();
             }
